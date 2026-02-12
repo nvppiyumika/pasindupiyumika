1cb6b1b46cca3d1e0b21203c64c2494a7a2bdc0b
Pasindu Piyumika <135522401+nvppiyumika@users.noreply.github.com>
2026-02-12 15:46:19 +0530

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  // Replace 'your-repo-name' with your actual repository name
+  basePath: '/pasindupiyumika',
+  // Optional: Unoptimized images work better on GitHub Pages without a custom loader
   images: {
     unoptimized: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+//export default nextConfig;
